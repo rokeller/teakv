530cd7e333281cfcf142b69fa4707ed83c857b82
Roger Keller <rokeller@users.noreply.github.com>
2024-09-07 17:21:44 +0200

    Implement entry scanning for `DefaultKeyValueStore`
    - Also add first set of unit tests

--- src/TeaSuite.KV/DefaultKeyValueStore.Scanning.cs
+++ src/TeaSuite.KV/DefaultKeyValueStore.Scanning.cs
@@ -11,13 +11,114 @@ partial class DefaultKeyValueStore<TKey, TValue>
     protected override IEnumerable<IEnumerator<StoreEntry<TKey, TValue>>> GetEntriesEnumerators(
         Range<TKey> range)
     {
-        throw new NotImplementedException();
+        // We need to acquire the read-lock before enumeration starts, so let's
+        // do that here.
+        IDisposable? readLock = lockingPolicy.AcquireReadLock();
+        IEnumerator<StoreEntry<TKey, TValue>> inMemEnumerator =
+            CreateInMemoryEnumerator(range, readLock);
+        IEnumerable<IEnumerator<StoreEntry<TKey, TValue>>> segmentEnumerators =
+            base.GetEntriesEnumerators(range);
+
+        // The enumerator for the in-memory entries must always be first in the
+        // list so as to make sure its entries take precendence over entries
+        // from persisted segments.
+        return segmentEnumerators.Prepend(inMemEnumerator);
     }
 
     /// <inheritdoc/>
     protected override IEnumerator<StoreEntry<TKey, TValue>> CreateEntriesEnumerator(
         IEnumerable<IEnumerator<StoreEntry<TKey, TValue>>> enumerators)
     {
-        throw new NotImplementedException();
+        IEnumerator<StoreEntry<TKey, TValue>> first = enumerators.First();
+
+        // If the first enumerator of all passed enumerators is our own
+        // InMemoryEnumerator, we need to extract the read locker from it so we
+        // can release it (through the GuardingEnumerator) once the enumerator
+        // is disposed.
+        if (first is InMemoryEnumerator inMemoryEnumerator)
+        {
+            IDisposable? readLock = inMemoryEnumerator.ReadLock;
+            if (null != readLock)
+            {
+                return new GuardingEnumerator<StoreEntry<TKey, TValue>>(
+                    readLock, base.CreateEntriesEnumerator(enumerators));
+            }
+        }
+
+        return base.CreateEntriesEnumerator(enumerators);
+    }
+
+    /// <summary>
+    /// Create an <see cref="InMemoryEnumerator"/> that enumerates the entries
+    /// from the in-memory store or stores in case there is an 'old' (being
+    /// written to disk) and a new one.
+    /// </summary>
+    /// <param name="range">
+    /// The <see cref="Range{T}"/> to enumerate.
+    /// </param>
+    /// <param name="readLock">
+    /// The <see cref="IDisposable"/> that represents the read lock or null if
+    /// a read lock is not used.
+    /// </param>
+    /// <returns>
+    /// An <see cref="InMemoryEnumerator"/> value that represents an
+    /// <see cref="IEnumerator{T}"/> to enumerate the entries from the in-memory
+    /// store(s).
+    /// </returns>
+    private InMemoryEnumerator CreateInMemoryEnumerator(
+        Range<TKey> range,
+        IDisposable? readLock)
+    {
+        IEnumerator<StoreEntry<TKey, TValue>> curEnumerator =
+            memoryStores.Current.GetOrderedEnumerator(range);
+        IEnumerator<StoreEntry<TKey, TValue>>? oldEnumerator =
+            memoryStores.Old?.GetOrderedEnumerator(range);
+
+        if (null != oldEnumerator)
+        {
+            return new InMemoryEnumerator(
+                readLock,
+                new MergingEnumerator<StoreEntry<TKey, TValue>>(
+                    curEnumerator, oldEnumerator));
+        }
+        else
+        {
+            return new InMemoryEnumerator(readLock, curEnumerator);
+        }
+    }
+
+    private readonly record struct InMemoryEnumerator :
+        IEnumerator<StoreEntry<TKey, TValue>>
+    {
+        private readonly IEnumerator<StoreEntry<TKey, TValue>> inner;
+
+        public InMemoryEnumerator(
+            IDisposable? readLock,
+            IEnumerator<StoreEntry<TKey, TValue>> inner)
+        {
+            ReadLock = readLock;
+            this.inner = inner;
+        }
+
+        public IDisposable? ReadLock { get; }
+
+        public StoreEntry<TKey, TValue> Current => inner.Current;
+
+        object IEnumerator.Current => ((IEnumerator)inner).Current;
+
+        public void Dispose()
+        {
+            inner.Dispose();
+        }
+
+        public bool MoveNext()
+        {
+            return inner.MoveNext();
+        }
+
+        public void Reset()
+        {
+            inner.Reset();
+        }
     }
 }
---
+++ test/TeaSuite.KV.UnitTests/DefaultKeyValueStoreTests.Scanning.cs
@@ -0,0 +1,179 @@
+using System.Collections;
+using System.Reflection;
+using AutoFixture.Xunit2;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using TeaSuite.KV.IO;
+using static TeaSuite.KV.StoreUtils;
+
+namespace TeaSuite.KV;
+
+partial class DefaultKeyValueStoreTests
+{
+    [Theory]
+    [InlineAutoData]
+    public void GetEntriesEnumeratorWrapsWithGardingEnumeratorForReadLock(int a, int b)
+    {
+        Range<int> range = new()
+        {
+            HasStart = true,
+            Start = a,
+            HasEnd = true,
+            End = b,
+        };
+        Mock<ISegmentManager<int, int>> mockSegmentManager = CreateSegmentManager();
+        DefaultKeyValueStore<int, int> store = new(
+            NullLogger<DefaultKeyValueStore<int, int>>.Instance,
+            NullWriteAheadLog<int, int>.Instance,
+            mockMemFactory.Object,
+            mockLockingPolicy.Object,
+            mockSegmentManager.Object,
+            mockOptions.Object,
+            mockClock.Object);
+
+        mockLockingPolicy.Setup(p => p.AcquireReadLock()).Returns(mockDisposable.Object);
+
+        Mock<IEnumerator<StoreEntry<int, int>>> mockInMemEnum = new(MockBehavior.Strict);
+        mockMemStore
+            .Setup(s => s.GetOrderedEnumerator(range))
+            .Returns(mockInMemEnum.Object);
+        mockInMemEnum.Setup(e => e.Dispose());
+
+        mockDisposable.Setup(d => d.Dispose());
+
+        IEnumerator<StoreEntry<int, int>> enumerator = store.GetEntriesEnumerator(range);
+        Assert.IsType<GuardingEnumerator<StoreEntry<int, int>>>(enumerator);
+
+        mockLockingPolicy.Verify(p => p.AcquireReadLock(), Times.Once);
+        mockDisposable.Verify(d => d.Dispose(), Times.Never);
+        mockInMemEnum.Verify(e => e.Dispose(), Times.Never);
+
+        enumerator.Dispose();
+
+        mockDisposable.Verify(d => d.Dispose(), Times.Once);
+        mockInMemEnum.Verify(e => e.Dispose(), Times.Once);
+    }
+
+    [Theory]
+    [InlineAutoData]
+    public void GetEntriesEnumeratorWrapsWithGardingEnumeratorForNullReadLock(int a, int b)
+    {
+        Range<int> range = new()
+        {
+            HasStart = true,
+            Start = a,
+            HasEnd = true,
+            End = b,
+        };
+        Mock<ISegmentManager<int, int>> mockSegmentManager = CreateSegmentManager();
+        DefaultKeyValueStore<int, int> store = new(
+            NullLogger<DefaultKeyValueStore<int, int>>.Instance,
+            NullWriteAheadLog<int, int>.Instance,
+            mockMemFactory.Object,
+            mockLockingPolicy.Object,
+            mockSegmentManager.Object,
+            mockOptions.Object,
+            mockClock.Object);
+
+        mockLockingPolicy.Setup(p => p.AcquireReadLock()).Returns((IDisposable?)null);
+
+        Mock<IEnumerator<StoreEntry<int, int>>> mockInMemEnum = new(MockBehavior.Strict);
+        mockMemStore
+            .Setup(s => s.GetOrderedEnumerator(range))
+            .Returns(mockInMemEnum.Object);
+        mockInMemEnum.Setup(e => e.Dispose());
+
+        IEnumerator<StoreEntry<int, int>> enumerator = store.GetEntriesEnumerator(range);
+        Assert.IsType<MergingEnumerator<StoreEntry<int, int>>>(enumerator);
+
+        mockLockingPolicy.Verify(p => p.AcquireReadLock(), Times.Once);
+        mockDisposable.Verify(d => d.Dispose(), Times.Never);
+        mockInMemEnum.Verify(e => e.Dispose(), Times.Never);
+
+        enumerator.Dispose();
+
+        mockDisposable.Verify(d => d.Dispose(), Times.Never);
+        mockInMemEnum.Verify(e => e.Dispose(), Times.Once);
+    }
+
+    [Theory]
+    [InlineAutoData]
+    public void InMemoryEnumeratorForwardsToInner(int key, int value)
+    {
+        Type storeType = typeof(DefaultKeyValueStore<int, int>);
+        Type enumeratorTypeGeneric = storeType.GetNestedType(
+            "InMemoryEnumerator", BindingFlags.NonPublic)!;
+        Type enumeratorType = enumeratorTypeGeneric.MakeGenericType(typeof(int), typeof(int));
+        System.Reflection.PropertyInfo readLockProp = enumeratorType.GetProperty("ReadLock")!;
+
+        Mock<IEnumerator<StoreEntry<int, int>>> mockInner = new(MockBehavior.Strict);
+        IEnumerator<StoreEntry<int, int>> enumerator =
+            (IEnumerator<StoreEntry<int, int>>)Activator.CreateInstance(
+                enumeratorType, mockDisposable.Object, mockInner.Object)!;
+
+        StoreEntry<int, int> entry = new(key, value);
+
+        mockInner.SetupSequence(e => e.MoveNext()).Returns(true).Returns(false);
+        mockInner.SetupSequence(e => e.Current)
+            .Returns(entry).Throws(new InvalidOperationException());
+        mockInner.As<IEnumerator>().SetupSequence(e => e.Current)
+            .Returns(entry).Throws(new InvalidOperationException());
+        mockInner.Setup(e => e.Reset());
+        mockInner.Setup(e => e.Dispose());
+
+        Assert.True(enumerator.MoveNext());
+        Assert.Equal(entry, enumerator.Current);
+        Assert.Equal(entry, ((IEnumerator)enumerator).Current);
+        Assert.False(enumerator.MoveNext());
+
+        enumerator.Reset();
+
+        Assert.Same(mockDisposable.Object, readLockProp.GetValue(enumerator));
+
+        enumerator.Dispose();
+
+        mockInner.Verify(e => e.MoveNext(), Times.Exactly(2));
+        mockInner.Verify(e => e.Current, Times.Once);
+        mockInner.As<IEnumerator>().Verify(e => e.Current, Times.Once);
+        mockInner.Verify(e => e.Reset(), Times.Once);
+        mockInner.Verify(e => e.Dispose(), Times.Once);
+    }
+
+    [Fact]
+    public void CreateEntriesEnumeratorSkipsWrappingIfNoInMemoryEnumeratorFirst()
+    {
+        Mock<ISegmentManager<int, int>> mockSegmentManager = CreateSegmentManager();
+        DefaultKeyValueStore<int, int> store = new(
+            NullLogger<DefaultKeyValueStore<int, int>>.Instance,
+            NullWriteAheadLog<int, int>.Instance,
+            mockMemFactory.Object,
+            mockLockingPolicy.Object,
+            mockSegmentManager.Object,
+            mockOptions.Object,
+            mockClock.Object);
+
+        Mock<IEnumerator<StoreEntry<int, int>>> mockInner = new(MockBehavior.Strict);
+#if NET6_0
+        List<IEnumerator<StoreEntry<int, int>>> enumerators = new()
+        {
+            mockInner.Object,
+        };
+#else
+        List<IEnumerator<StoreEntry<int, int>>> enumerators = [mockInner.Object,];
+#endif
+
+        Type storeType = store.GetType();
+        MethodInfo? method = storeType.GetMethod(
+            "CreateEntriesEnumerator", BindingFlags.NonPublic | BindingFlags.Instance);
+        Assert.NotNull(method);
+
+#if NET6_0
+        IEnumerator<StoreEntry<int, int>> result =
+            (IEnumerator<StoreEntry<int, int>>)method.Invoke(store, new object[] {enumerators,})!;
+#else
+        IEnumerator<StoreEntry<int, int>> result =
+            (IEnumerator<StoreEntry<int, int>>)method.Invoke(store, [enumerators,])!;
+#endif
+        Assert.IsType<MergingEnumerator<StoreEntry<int, int>>>(result);
+    }
+}
